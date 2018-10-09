3aecdf8842a7dc13546ca9bc60e30bc33957f47e
Nick McMillan <nick.a.mcmillan@gmail.com>
2018-10-09 16:19:24 +0200

    added raycasting

--- src/index.js
+++ src/index.js
@@ -6,12 +6,26 @@ import './index.css';
 
 var isMobile = false
 var antialias = true
-var grd = null
 
-const MAX = 10
 
-var raycaster, mouse = { x: 0, y: 0 };
+const max = 1
+var PI2 = Math.PI * 2;
 
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2()
+
+// var particleMaterial = new THREE.SpriteCanvasMaterial({
+
+//   color: 0x000000,
+//   program: function (context) {
+
+//     context.beginPath();
+//     context.arc(0, 0, 0.5, 0, PI2, true);
+//     context.fill();
+
+//   }
+
+// });
 
 
 // three var
@@ -30,96 +44,155 @@ var infos;
 // var type=1;
 init();
 loop();
-function init() {
-    var n = navigator.userAgent;
-    if (n.match(/Android/i) || n.match(/webOS/i) || n.match(/iPhone/i) || n.match(/iPad/i) || n.match(/iPod/i) || n.match(/BlackBerry/i) || n.match(/Windows Phone/i)){ isMobile = true;  antialias = false; }
-    infos = document.getElementById("info");
-    canvas = document.getElementById("canvas");
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.set( 0, 160, 400 );
-    //controls = new THREE.OrbitControls( camera, canvas );
-    // controls.target.set(0, 20, 0);
-    // controls.update();
-    scene = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer({ canvas:canvas, precision: "mediump", antialias:antialias });
-    renderer.setSize( window.innerWidth, window.innerHeight );
 
-    raycaster = new THREE.Raycaster();
-    renderer.domElement.addEventListener('click', raycast, false);
+function addBlocks() {
+  var geometry = new THREE.BoxBufferGeometry(100, 100, 100);
 
+  var objects = [];
+  for (var i = 0; i < 5; i++) {
 
+    var object = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: Math.random() * 0xffffff, opacity: 0.5 }));
+    object.position.x = Math.random() * 800 - 400;
+    object.position.y = Math.random() * 800 - 400;
+    object.position.z = Math.random() * 800 - 400;
 
-    var materialType = 'MeshBasicMaterial';
-    
-    if (!isMobile){
-        scene.add( new THREE.AmbientLight( 0x3D4143 ) );
-        light = new THREE.DirectionalLight( 0xffffff , 1);
-        light.position.set( 300, 1000, 500 );
-        light.target.position.set( 0, 0, 0 );
-        light.castShadow = true;
-        var d = 300;
-        light.shadow.camera = new THREE.OrthographicCamera( -d, d, d, -d,  500, 1600 );
-        light.shadow.bias = 0.0001;
-        light.shadow.mapSize.width = light.shadow.mapSize.height = 1024;
-        scene.add( light );
-        materialType = 'MeshPhongMaterial';
-        renderer.shadowMap.enabled = true;
-        renderer.shadowMap.type = THREE.PCFShadowMap;//THREE.BasicShadowMap;
-    }
-    // background
-    var buffgeoBack = new THREE.BufferGeometry();
-    buffgeoBack.fromGeometry( new THREE.IcosahedronGeometry(8000,1) );
-    var back = new THREE.Mesh( buffgeoBack, new THREE.MeshBasicMaterial( { map:gradTexture([[1,0.75,0.5,0.25], ['#1B1D1E','#3D4143','#72797D', '#b0babf']]), side:THREE.BackSide, depthWrite: false }  ));
-    back.geometry.applyMatrix(new THREE.Matrix4().makeRotationZ(15*ToRad));
-    scene.add( back );
-    geoBox = new THREE.BoxGeometry( 1, 1, 1 );
-    geoCyl = new THREE.CylinderGeometry( 0.5, 0.5, 1, 6, 1 );
-    buffgeoSphere = new THREE.BufferGeometry();
-    buffgeoSphere.fromGeometry( new THREE.SphereGeometry( 1 , 20, 10 ) );
-    buffgeoBox = new THREE.BufferGeometry();
-    buffgeoBox.fromGeometry( new THREE.BoxGeometry( 1, 1, 1 ) );
-    matSphere = new THREE[materialType]( { map: basicTexture(0), name:'sph' ,specular: 0xFFFFFF, shininess: 120, transparent: true, opacity: 0.9 } );
-    matBox = new THREE[materialType]( {  map: basicTexture(2), name:'box' } );
-    matSphereSleep = new THREE[materialType]( { map: basicTexture(1), name:'ssph', specular: 0xFFFFFF, shininess: 120 , transparent: true, opacity: 0.7} );
-    matBoxSleep = new THREE[materialType]( {  map: basicTexture(3), name:'sbox' } );
-    matGround = new THREE[materialType]( { shininess: 10, color:0x3D4143, transparent:true, opacity:0.5 } );
-    // events
-    window.addEventListener( 'resize', onWindowResize, false );
-    // physics
-    initOimoPhysics();
+    object.scale.x = Math.random() * 2 + 1;
+    object.scale.y = Math.random() * 2 + 1;
+    object.scale.z = Math.random() * 2 + 1;
+
+    object.rotation.x = Math.random() * 2 * Math.PI;
+    object.rotation.y = Math.random() * 2 * Math.PI;
+    object.rotation.z = Math.random() * 2 * Math.PI;
+
+    scene.add(object);
+
+    objects.push(object);
+
+  }
 }
 
-function raycast(e) {
+function init() {
+  var n = navigator.userAgent;
+  if (n.match(/Android/i) || n.match(/webOS/i) || n.match(/iPhone/i) || n.match(/iPad/i) || n.match(/iPod/i) || n.match(/BlackBerry/i) || n.match(/Windows Phone/i)) { isMobile = true;  antialias = false; }
+  infos = document.getElementById("info");
+  canvas = document.getElementById("canvas");
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.set(0, 300, 500);
+  //controls = new THREE.OrbitControls( camera, canvas );
+  // controls.target.set(0, 20, 0);
+  // controls.update();
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
+  scene.fog = new THREE.Fog(0x050505, 2000, 3500)
   
-  // Step 2: Detect normal objects
-  //1. sets the mouse position with a coordinate system where the center
-  //   of the screen is the origin
-  mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
+  // scene.add(new THREE.AmbientLight(0x444444))
 
-  //2. set the picking ray from the camera position and mouse coordinates
+  renderer = new THREE.WebGLRenderer({ canvas:canvas, precision: "mediump", antialias:antialias });
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  addBlocks()
+  
+
+  var materialType = 'MeshBasicMaterial';
+  
+  if (!isMobile) {
+      scene.add( new THREE.AmbientLight( 0x3D4143 ) );
+      light = new THREE.DirectionalLight( 0xffffff , 1);
+      light.position.set( 300, 1000, 500 );
+      light.target.position.set( 0, 0, 0 );
+      light.castShadow = true;
+      var d = 300;
+      light.shadow.camera = new THREE.OrthographicCamera( -d, d, d, -d,  500, 1600 );
+      light.shadow.bias = 0.0001;
+      light.shadow.mapSize.width = light.shadow.mapSize.height = 1024;
+      scene.add( light );
+      materialType = 'MeshPhongMaterial';
+      renderer.shadowMap.enabled = true;
+      renderer.shadowMap.type = THREE.PCFShadowMap;//THREE.BasicShadowMap;
+  }
+  
+  geoBox = new THREE.BoxGeometry( 1, 1, 1 );
+  geoCyl = new THREE.CylinderGeometry( 0.5, 0.5, 1, 6, 1 );
+  buffgeoSphere = new THREE.BufferGeometry();
+  buffgeoSphere.fromGeometry( new THREE.SphereGeometry( 1 , 20, 10 ) );
+  buffgeoBox = new THREE.BufferGeometry();
+  buffgeoBox.fromGeometry( new THREE.BoxGeometry( 1, 1, 1 ) );
+  matSphere = new THREE[materialType]( { map: basicTexture(0), name:'sph' ,specular: 0xFFFFFF, shininess: 120, transparent: true, opacity: 0.9 } );
+  matBox = new THREE[materialType]( {  map: basicTexture(2), name:'box' } );
+  matSphereSleep = new THREE[materialType]( { map: basicTexture(1), name:'ssph', specular: 0xFFFFFF, shininess: 120 , transparent: true, opacity: 0.7} );
+  matBoxSleep = new THREE[materialType]( {  map: basicTexture(3), name:'sbox' } );
+  matGround = new THREE[materialType]( { shininess: 10, color:'pink', transparent: false, opacity:0.5 } );
+  // events
+  window.addEventListener( 'resize', onWindowResize, false );
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
+  renderer.domElement.addEventListener('click', raycast2, false);
+  
+  // physics
+  initOimoPhysics();
+}
+function onDocumentMouseMove(event) {
+
+  event.preventDefault();
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+}
+
+function raycast2(event) {
+
+  event.preventDefault();
+
+  mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+  mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+
   raycaster.setFromCamera(mouse, camera);
 
-  //3. compute intersections (no 2nd parameter true anymore)
   var intersects = raycaster.intersectObjects(scene.children);
 
-  for (var i = 0; i < intersects.length; i++) {
-    console.log(intersects[i]);
-    /*
-        An intersection has the following properties :
-            - object : intersected object (THREE.Mesh)
-            - distance : distance from camera to intersection (number)
-            - face : intersected face (THREE.Face3)
-            - faceIndex : intersected face index (number)
-            - point : intersection point (THREE.Vector3)
-            - uv : intersection point in the object's UV coordinates (THREE.Vector2)
-    */
+  if (intersects.length > 0) {
+
+    intersects[0].object.material.color.setHex(Math.random() * 0xffffff);
+
+    const geometry = new THREE.SphereGeometry(5, 32, 32)
+    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+
+    const particle = new THREE.Mesh(geometry, material)
+    particle.position.copy(intersects[0].point)
+    scene.add(particle)
   }
 
 }
 
+
+var theta = 0
+var radius = 600;
+
 function loop() {
     updateOimoPhysics();
+    raycaster.setFromCamera(mouse, camera);
+    // var intersects = raycaster.intersectObject();
+
+    // if (intersects.length > 0) {
+
+    //   var intersect = intersects[0];
+    //   var face = intersect.face;
+
+    //   console.log(face)
+      
+
+    // } else {
+      
+
+    // }
+      
+    theta += 0.1;
+
+    camera.position.x = radius * Math.sin(THREE.Math.degToRad(theta));
+    camera.position.y = radius * Math.sin(THREE.Math.degToRad(theta));
+    camera.position.z = radius * Math.cos(THREE.Math.degToRad(theta));
+    camera.lookAt(scene.position);
     renderer.render( scene, camera );
     requestAnimationFrame( loop );
 }
@@ -159,14 +232,6 @@ function initChairGeometry() {
     chairGeometry.fromGeometry( g );
 }
 
-// function clearMesh() {
-//     var i = meshs.length;
-//     while (i--) scene.remove(meshs[ i ]);
-//     i = grounds.length;
-//     while (i--) scene.remove(grounds[ i ]);
-//     grounds = [];
-//     meshs = [];
-// }
 //----------------------------------
 //  OIMO PHYSICS
 //----------------------------------
@@ -178,19 +243,16 @@ function initOimoPhysics() {
 }
 
 function populate() {
-  var max = MAX
   var pos = [];
-  // clearMesh();
   world.clear();
   
-  bodies=[];
+  bodies = [];
   var b;
-  var ground = world.add({size:[100, 40, 390], pos:[0,-20,0], world:world});
-  // var ground2 = world.add({size:[400, 40, 400], pos:[0,-60,0], world:world});
+  // the floor physics
+  world.add({size:[100, 40, 390], pos:[0,-20,0], world:world});
   
-  // the floor
+  // the floor appearance
   addStaticBox([100, 40, 390], [0,-20,0], [0,0,0]);
-  // addStaticBox([400, 40, 400], [0,-60,0], [0,0,0]);
   var i = max;
   var j;
   while (i--) {
@@ -229,7 +291,7 @@ function updateOimoPhysics() {
     while (i--) {
         body = bodies[i];
         mesh = meshs[i];
-        if (!body.sleeping){
+        if (!body.sleeping) {
             // apply physics mouvement
             mesh.position.copy(body.getPosition());
             mesh.quaternion.copy(body.getQuaternion());
@@ -238,7 +300,7 @@ function updateOimoPhysics() {
             if (mesh.material.name === 'ssph') mesh.material = matSphere; 
             // reset position
             
-            if (mesh.position.y < -100){
+            if (mesh.position.y < -100) {
                 x = -100 + Math.random() * 200
                 z = -100 + Math.random() * 200
                 y = 100 + Math.random() * 1000
@@ -256,46 +318,48 @@ function updateOimoPhysics() {
 //  TEXTURES
 //----------------------------------
 function gradTexture(color) {
-    var c = document.createElement("canvas");
-    var ct = c.getContext("2d");
-    c.width = 16; c.height = 256;
-    var gradient = ct.createLinearGradient(0,0,0,256);
-    var i = color[0].length;
-    while(i--){ gradient.addColorStop(color[0][i],color[1][i]); }
-    ct.fillStyle = gradient;
-    ct.fillRect(0,0,16,256);
-    var texture = new THREE.Texture(c);
-    texture.needsUpdate = true;
-    return texture;
+  var c = document.createElement("canvas");
+  var ct = c.getContext("2d");
+  c.width = 16; c.height = 256;
+  var gradient = ct.createLinearGradient(0,0,0,256);
+  var i = color[0].length;
+  while(i--) { gradient.addColorStop(color[0][i],color[1][i]); }
+  ct.fillStyle = gradient;
+  ct.fillRect(0,0,16,256);
+  var texture = new THREE.Texture(c);
+  texture.needsUpdate = true;
+  return texture;
 }
-function basicTexture(n){
-    var canvas = document.createElement( 'canvas' );
-    canvas.width = canvas.height = 64;
-    var ctx = canvas.getContext( '2d' );
-    var colors = [];
-    if (n===0){ // sphere
-        colors[0] = "#99999A";
-        colors[1] = "#cccccD";
-    }
-    if (n===1){ // sphere sleep
-        colors[0] = "#666667";
-        colors[1] = "#99999A";
-    }
-    if (n===2){ // box
-        colors[0] = "#AA8058";
-        colors[1] = "#FFAA58";
-    }
-    if (n===3){ // box sleep
-        colors[0] = "#383838";
-        colors[1] = "#AA8038";
-    }
-    grd=ctx.createLinearGradient(0,0,0,64);
-    grd.addColorStop(0,colors[1]);
-    grd.addColorStop(1,colors[0]);
 
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, 64, 64);
-    var tx = new THREE.Texture(canvas);
-    tx.needsUpdate = true;
-    return tx;
+function basicTexture(n) {
+  var canvas = document.createElement( 'canvas' );
+  canvas.width = canvas.height = 64;
+  var ctx = canvas.getContext( '2d' );
+  var colors = [];
+  if (n===0) { // sphere
+      colors[0] = "#99999A";
+      colors[1] = "#cccccD";
+  }
+  if (n===1) { // sphere sleep
+      colors[0] = "#666667";
+      colors[1] = "#99999A";
+  }
+  if (n===2) { // box
+      colors[0] = "#AA8058";
+      colors[1] = "#FFAA58";
+  }
+  if (n===3) { // box sleep
+      colors[0] = "#383838";
+      colors[1] = "#AA8038";
+  }
+  var grd = null
+  grd = ctx.createLinearGradient(0, 0, 0, 64);
+  grd.addColorStop(0, colors[1]);
+  grd.addColorStop(1, colors[0]);
+
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, 64, 64);
+  var tx = new THREE.Texture(canvas);
+  tx.needsUpdate = true;
+  return tx;
 }
