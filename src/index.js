4786c9c8654e904d585c8d092bf881650c509ba3
Nick McMillan <nick.a.mcmillan@gmail.com>
2018-10-21 19:18:55 +0200

    🚧 loadmodel feature

--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ import guitarObj from './models/guitar.obj'
 
 import { addJointBody } from './utils/handleJoints'
 import { onMouseMove, onMouseDown, onMouseUp } from './utils/handleInputs'
+import { onWindowResize } from './utils/handleResize'
+import { loadModel } from './utils/loadModel'
 
 import './index.css'
-const mtlLoader = new MTLLoader();
 const objLoader = new OBJLoader();
 
 
@@ -30,7 +31,10 @@ const dt = 1 / 60;
 // Apsect – We’re simply dividing the browser width and height to get an aspect ratio.
 // Near – This is the distance at which the camera will start rendering scene objects.
 // Far – Anything beyond this distance will not be rendered.Perhaps more commonly known as the draw distance.
-var renderer;
+export const renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById('canvas'),
+  antialias: true,
+})
 
 export const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.5, 100);
 export const scene = new THREE.Scene()
@@ -41,42 +45,36 @@ var invisoMaterial = new THREE.MeshLambertMaterial({ transparent: true, opacity:
 export const gplane = new THREE.Mesh(planeGeo, invisoMaterial);
 
 // stabalise
-gplane.position.copy(0,0,0)
-gplane.quaternion.copy(0,0,0)
+// gplane.position.copy(0,0,0)
+// gplane.quaternion.copy(0,0,0)
 // gplane.visible = false
 
 var geometry, material, mesh;
 
-const blockCount = 3;
-const keyboardCount = 2
+export const blockCount = 3;
+export const keyboardCount = 2
 
 // To be synced
 export const meshes = [] // threejs
 export const bodies = [] // cannon
 
-initCannon();
-init();
-animate();
 
-async function init() {
 
-  // scene
-  
-  scene.fog = new THREE.Fog(0xffffff, 30, 200);
-
+const init = function () {
   // camera
-  
   camera.position.set(20, 60, 20);
   camera.quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2);
-  scene.add(camera);
+
+  // scene
+  scene.fog = new THREE.Fog(0xffffff, 30, 200)
+  scene.add(camera)
 
   // lights
-
   scene.add(new THREE.AmbientLight(0x666666));
-  scene.add(gplane);
+  
 
   const light = new THREE.DirectionalLight(0xffffff, 1.25);
-  var d = 20;
+  const d = 20
 
   light.position.set(d, d, d);
 
@@ -93,17 +91,18 @@ async function init() {
   light.shadow.camera.far = 3 * d;
 
   scene.add(light);
+  scene.add(gplane);
 
   // floor
   geometry = new THREE.PlaneGeometry(100, 100, 1, 1);
   //geometry.applyMatrix( new THREE.Matrix4().makeRotationX( -Math.PI / 2 ) );
-  material = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  material = new THREE.MeshLambertMaterial({ color: 'pink' });
   
   // THREE.ColorUtils.adjustHSV( material.color, 0, 0, 0.9 );
   mesh = new THREE.Mesh(geometry, material);
   mesh.castShadow = true;
   mesh.quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 2);
-  mesh.receiveShadow = true;
+  mesh.receiveShadow = true
   scene.add(mesh);
 
   // cubes
@@ -111,41 +110,44 @@ async function init() {
   const cubeMaterial = new THREE.MeshPhongMaterial({ color: 0xbad455 });
 
   for (let i = 0; i < blockCount; i++) {
-
-    const cubeMesh = new THREE.Mesh(cubeGeo, cubeMaterial);
-    cubeMesh.castShadow = true;
-    meshes.push(cubeMesh);
-    scene.add(cubeMesh);
+    const cubeMesh = new THREE.Mesh(cubeGeo, cubeMaterial)
+    cubeMesh.castShadow = true
+    meshes.push(cubeMesh)
+    scene.add(cubeMesh)
   }
+
+  loadModel({
+    mtl: keyboardMtl,
+    obj: keyboardObj,
+    quantity: 2,
+    offsets: {
+      x: -0.75,
+      y: -0.3,
+      z: 0,
+    },
+    position: {
+      x: Math.random() * 2,
+      y: Math.random() * 60,
+      z: Math.random() * 2,
+    }
+  })
   
-  mtlLoader.load(keyboardMtl, materials => {
-    
-    materials.preload()
-    objLoader.setMaterials(materials)
-
-    objLoader.load(keyboardObj, (object) => {
-
-      object.traverse(node => {
-        if (node instanceof THREE.Mesh) {
-          node.castShadow = true
-          node.receiveShadow = true
-        }
-      })
-
-      for (var i = 0; i < keyboardCount; i++) {
-        const newObj = object.clone()       
-
-        meshes.push(newObj)
-        scene.add(newObj)
-      }
-    })
+  loadModel({
+    mtl: guitarMtl,
+    obj: guitarObj,
+    quantity: 2,
+    offsets: {
+      x: -0.2,
+      y: -4.8,
+      z: 0.1,
+    },
+    position: {
+      x: Math.random() * 2,
+      y: Math.random() * 60,
+      z: Math.random() * 2,
+    }
   })
-
-
-  renderer = new THREE.WebGLRenderer({ 
-    canvas: document.getElementById('canvas'),
-    antialias: true,
-  })
+  
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setClearColor(scene.fog.color);
@@ -155,26 +157,17 @@ async function init() {
   renderer.gammaOutput = true;
   renderer.shadowMap.enabled = true;
 
-  window.addEventListener('resize', onWindowResize, false);
+  window.addEventListener('resize', onWindowResize, false)
 
-  window.addEventListener("mousemove", onMouseMove, false);
-  window.addEventListener("mousedown", onMouseDown, false);
-  window.addEventListener("mouseup", onMouseUp, false);
+  window.addEventListener('mousemove', onMouseMove, false)
+  window.addEventListener('mousedown', onMouseDown, false)
+  window.addEventListener('mouseup', onMouseUp, false)
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enabled = false;
   controls.maxDistance = 1500;
   controls.minDistance = 0;
 
-  cannonDebugRenderer = new CannonDebugRenderer(scene, world);
-  
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  //controls.handleResize();
-  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 
@@ -188,10 +181,10 @@ function animate() {
 
 
 function updatePhysics() {
-  world.step(dt);
-  for (var i = 0; i !== meshes.length; i++) {    
-    meshes[i].position.copy(bodies[i].position);
-    meshes[i].quaternion.copy(bodies[i].quaternion);
+  world.step(dt)
+  for (var i = 0; i < meshes.length; i++) {
+    meshes[i].position.copy(bodies[i].position)
+    meshes[i].quaternion.copy(bodies[i].quaternion)
   }
 }
 
@@ -199,7 +192,7 @@ let theta = 30
 const radius = 20
 
 function render() {
-  theta += 0.1
+  theta += 0.04
 
   // if (draggedItem) {
   //   const pos = draggedItem.point
@@ -226,17 +219,18 @@ function render() {
   
 }
 
-const objectLoader = () => {
-  
-}
 
 function initCannon() {
   // Setup our world
   world = new CANNON.World();
   world.quatNormalizeSkip = 0;
   world.quatNormalizeFast = false;
+  // world.solver.iterations = 2
+  world.defaultContactMaterial.contactEquationStiffness = 1e8;
+  // world.defaultContactMaterial.contactEquationRegularizationTime = 3;
 
-  world.gravity.set(0, -16, 0);
+
+  world.gravity.set(0, -20, 0);
   world.broadphase = new CANNON.NaiveBroadphase();
 
   // Create boxes
@@ -249,33 +243,37 @@ function initCannon() {
     bodies.push(boxBody);
   }
 
-  objLoader.load(keyboardObj, (object) => {
 
-    // const keyboardShapeold = new CANNON.Box(new CANNON.Vec3(5, 1, 9))
-    
-    const keyboardShape = threeToCannon(object);
-    for (var i = 0; i < keyboardCount; i++) {
-      const keyboardBody = new CANNON.Body({ mass: 0.5 })
-
+  // const keyboardShape = new CANNON.Box(new CANNON.Vec3(1.5, 0.3, 3), )
   
-      keyboardBody.addShape(keyboardShape);
-      keyboardBody.position.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
-  
-      // keyboardBody.quaternion.setFromAxisAngle(new CANNON.Vec3(3, 4, 10), 1);
-      world.add(keyboardBody);
-      bodies.push(keyboardBody);
-    }
+  // // const keyboardShape = threeToCannon(object);
+  // for (var i = 0; i < keyboardCount; i++) {
+  //   const keyboardBody = new CANNON.Body({ mass: 5, shape: keyboardShape } )
 
-  })
+  //   // keyboardBody.position.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+
+  //   // keyboardBody.quaternion.setFromAxisAngle(new CANNON.Vec3(3, 4, 10), 1);
+  //   world.add(keyboardBody);
+  //   bodies.push(keyboardBody);
+  // }
+
+
 
 
   // Create a plane
   const groundShape = new CANNON.Plane();
-  const groundBody = new CANNON.Body({ mass: 0 });
+  const groundBody = new CANNON.Body({ mass: 0 }) // ground has mass = 0 which makes it static
   groundBody.addShape(groundShape);
   groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
   world.add(groundBody);
 
   addJointBody()
 
+  cannonDebugRenderer = new CannonDebugRenderer(scene, world);
+
 }
+
+
+init()
+initCannon()
+animate()
---
+++ src/utils/loadModel.js
@@ -0,0 +1,77 @@
+import * as THREE from 'three'
+import * as CANNON from 'cannon'
+import { MTLLoader, OBJLoader } from 'three-obj-mtl-loader'
+
+import { meshes, scene, world, bodies } from '../index'
+
+
+
+function promisifyLoader(loader, onProgress) {
+  function promiseLoader(url) {
+
+    return new Promise((resolve, reject) => {
+      loader.load(url, resolve, onProgress, reject)
+    })
+  }
+
+  return {
+    originalLoader: loader,
+    load: promiseLoader,
+  }
+}
+
+
+export const loadModel = async ({mtl, obj, quantity, position, offsets}) => {
+
+  const mtlLoader = new MTLLoader()
+  const objLoader = new OBJLoader()
+
+  const mtlPromiseLoader = promisifyLoader(mtlLoader)
+  const materials = await mtlPromiseLoader.load(mtl).then(mtl => mtl)
+
+  materials.preload()
+  objLoader.setMaterials(materials)
+
+  const objPromiseLoader = promisifyLoader(objLoader)
+  const object = await objPromiseLoader.load(obj).then(obj => obj)
+  
+  object.traverse(child => {
+    child.position.x = offsets.x || 0
+    child.position.y = offsets.y || 0
+    child.position.z = offsets.z || 0
+    if (child instanceof THREE.Mesh) {
+      child.castShadow = true
+      child.receiveShadow = true
+    }
+  })
+
+  // get dimensions from three
+  const tempBox = new THREE.Box3().setFromObject(object)
+  const { x, y, z } = tempBox.getSize()
+  // apply dimensions to cannon
+  const shape = new CANNON.Box(new CANNON.Vec3(x/2, y/2, z/2))
+  
+  for (var i = 0; i < quantity; i++) {
+    
+    let clonedObj = object
+    if (i !== 0) clonedObj = object.clone() // if its looping, start cloning
+    
+    // add to threejs
+    meshes.push(clonedObj)
+    scene.add(clonedObj)
+    
+    // add to cannon
+    const body = new CANNON.Body({
+      mass: 50,
+      shape,
+      // fixedRotation: true,
+      linearDamping: 0.2,
+    })
+
+    body.position.set(position.x, position.y, position.z, );
+
+    world.add(body)
+    bodies.push(body)
+
+  }
+}
