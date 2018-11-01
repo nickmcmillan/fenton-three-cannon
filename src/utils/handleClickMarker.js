a43f2c56759ddb807e4425c2492cc2e59a01eaf3
Nick McMillan <nick.a.mcmillan@gmail.com>
2018-11-01 23:35:51 +0100

    🚧 initClickMarker

--- src/utils/handleClickMarker.js
+++ src/utils/handleClickMarker.js
@@ -1,13 +1,16 @@
 import * as THREE from 'three'
 import { scene } from '../index'
 
+
 const shape = new THREE.SphereGeometry(0.2, 8, 8)
 const markerMaterial = new THREE.MeshLambertMaterial({ color: 0x00ccdd })
 const clickMarker = new THREE.Mesh(shape, markerMaterial)
 clickMarker.visible = false
 
+export const initClickMarker = function () {
+  scene.add(clickMarker)
+}
 export const setClickMarker = function (x, y, z) {
-  if (!clickMarker.visible) scene.add(clickMarker)
   clickMarker.visible = true
   clickMarker.position.set(x, y, z)
 }
