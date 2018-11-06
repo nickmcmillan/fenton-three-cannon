dba7073beba2e74a3b47de41bab3a34ab854426d
Nick McMillan <nick.a.mcmillan@gmail.com>
2018-11-06 21:24:28 +0100

    🚧 ground

--- src/add/addGround.js
+++ src/add/addGround.js
@@ -5,12 +5,20 @@ import { scene, world } from '../index'
 
 export const addGround = function () {
   // THREE
-  const geo = new THREE.PlaneGeometry(10, 10)
+  const geo = new THREE.PlaneGeometry(15, 15)
   const mat = new THREE.MeshLambertMaterial({ color: 'blue' })
   const mesh = new THREE.Mesh(geo, mat)
   mesh.quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 2)
   mesh.receiveShadow = true
   scene.add(mesh)
+
+  const geo2 = new THREE.PlaneGeometry(100, 100)
+  const mat2 = new THREE.MeshLambertMaterial({ color: 'white' })
+  const mesh2 = new THREE.Mesh(geo2, mat2)
+  mesh2.quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 2)
+  mesh2.position.y = -0.1
+  mesh2.receiveShadow = true
+  scene.add(mesh2)
 
 
   // CANNON
