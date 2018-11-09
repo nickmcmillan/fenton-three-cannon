3e361b8ae397fefba1890d0a6b3c47d35b56f531
Nick McMillan <nick.a.mcmillan@gmail.com>
2018-11-09 11:48:05 +0100

    🐛 added target for dragPlane.intersectLine

--- src/utils/updateDragPosition.js
+++ src/utils/updateDragPosition.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three'
 import getCameraRay from './getCameraRay'
-import { setClickMarker } from './handleClickMarker'
+// import { setClickMarker } from './handleClickMarker'
 import { lastPos } from './handleInputs'
 import { mouseConstraint, moveJointToPoint } from './handleJoints'
 import { dragPlane } from '../index'
+
+const tempVec3 = new THREE.Vector3()
 
 export default function() {
    
@@ -14,10 +16,14 @@ export default function() {
       new THREE.Line3(
         ray.origin,
         ray.origin.clone().add(ray.direction.clone().multiplyScalar(10000))
-      )
+      ),
+      // THREE now requires a target
+      // https://github.com/mrdoob/three.js/issues/12231
+      // We create it once (above) outside this function
+      tempVec3
     )
 
-    setClickMarker(pos.x, pos.y, pos.z)
+    // setClickMarker(pos.x, pos.y, pos.z)
     moveJointToPoint(pos.x, pos.y, pos.z)
   }
 }
