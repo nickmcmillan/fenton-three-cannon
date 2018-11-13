4fcc306256b9731dd8f766940dddbe9485c8de4d
Nick McMillan <nick.a.mcmillan@gmail.com>
2018-11-13 23:05:32 +0100

    🐛 thicker ground

--- src/add/addGround.js
+++ src/add/addGround.js
@@ -28,9 +28,9 @@ export const addGround = function () {
   // CANNON
   // Create a plane
   // const shape = new CANNON.Plane() 
-  const shape = new CANNON.Box(new CANNON.Vec3(bounds / 2, bounds / 2, 0.1))
+  const shape = new CANNON.Box(new CANNON.Vec3(bounds / 2, bounds / 2, 1))
   const body = new CANNON.Body({ mass: 0, shape }) // mass = 0 which makes it static
-  body.position.set(0, -0.2, 0)
+  body.position.set(0, -1, 0)
   body.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
   world.add(body)
 }
