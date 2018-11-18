ffafe763c8bfb015ebf95a1d97f1723ceefdce45
Nick McMillan <nick.a.mcmillan@gmail.com>
2018-11-18 22:31:21 +0100

    🔧 adding more dat.gui controls

--- src/cannon.js
+++ src/cannon.js
@@ -1,5 +1,6 @@
 import * as CANNON from 'cannon'
-import { GUI } from 'dat.gui'
+
+import { settings } from './index'
 // import { CannonDebugRenderer } from './helpers/cannonDebugRenderer'
 import { meshes, /* scene */ } from './three'
 
@@ -7,15 +8,8 @@ export const world = new CANNON.World()
 export const bodies = []
 // export let cannonDebugRenderer
 
-const gui = new GUI()
-const timeStep = 1 / 60
-const settings = {
-  gx: 0,
-  gy: -40,
-  gz: 0,
-  restitution: 0.5,
-}
 
+const timeStep = 1 / 60
 export const updatePhysics = function () {
   world.step(timeStep)
   for (var i = 0; i < meshes.length; i++) {
@@ -45,25 +39,10 @@ export default function () {
   // world.solver.iterations = 2
   world.defaultContactMaterial.contactEquationRelaxation = 3
   world.defaultContactMaterial.contactEquationStiffness = 1e8
-  world.defaultContactMaterial.restitution = 0.5
+  world.defaultContactMaterial.restitution = settings.restitution
   world.defaultContactMaterial.friction = 0.1  
   // world.defaultContactMaterial.contactEquationRegularizationTime = 3
 
-  // add dat.gui bindings
-  gui.add(settings, 'gx', -40, 40).onChange(function(val) {
-    if (!isNaN(val)) world.gravity.set(val, settings.gy, settings.gz)
-  })
-  gui.add(settings, 'gy', -40, 1).onChange(function(val) {
-    if (!isNaN(val)) world.gravity.set(settings.gx, val, settings.gz)
-  })
-  gui.add(settings, 'gz', -40, 40).onChange(function(val) {
-    if (!isNaN(val)) world.gravity.set(settings.gx, settings.gy, val)
-  })
-  gui.add(settings, 'restitution', 0.5, 2).onChange(function(val) {
-    if (!isNaN(val)) world.defaultContactMaterial.restitution = val
-  })
-
-  gui.close()
   
   world.gravity.set(settings.gx, settings.gy, settings.gz)
   world.broadphase = new CANNON.NaiveBroadphase()
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { Math as MathThree } from 'three'
-
+import { GUI } from 'dat.gui'
 // import { SMAAEffect, EffectComposer, EffectPass, RenderPass } from 'postprocessing'
 
-import initCannon, { updatePhysics, /*cannonDebugRenderer*/ } from './cannon'
+import initCannon, { updatePhysics, world /*cannonDebugRenderer*/ } from './cannon'
 import initThree, { camera, scene, renderer } from './three'
 import addItems from './addItems'
 // import stats from './helpers/stats'
@@ -13,6 +13,7 @@ import updateDragPosition from './utils/updateDragPosition'
 import './index.scss'
 import * as serviceWorker from './serviceWorker';
 
+const gui = new GUI()
 
 // const clock = new Clock()
 // const composer = new EffectComposer(renderer)
@@ -22,15 +23,30 @@ import * as serviceWorker from './serviceWorker';
 // composer.addPass(effectPass)
 
 
-let theta = 15
-const radius = 20
+// let theta = 15
+// const radius = 20
+
+export const settings = {
+  // physics
+  gx: 0,
+  gy: -40,
+  gz: 0,
+  restitution: 0.5,
+  // camera
+  theta: 15,
+  radius: 20,
+  height: 2.5,
+  autoRotate: true,
+}
 
 const render = function () {
-  theta += 0.06
-  
-  camera.position.x = radius * Math.sin(MathThree.degToRad(theta))
-  camera.position.y = MathThree.degToRad(360 * 2.5)
-  camera.position.z = radius * Math.cos(MathThree.degToRad(theta))
+
+  if (settings.autoRotate) {
+    settings.theta += 0.06    
+  }
+  camera.position.x = settings.radius * Math.sin(MathThree.degToRad(settings.theta))
+  camera.position.y = MathThree.degToRad(360 * settings.height)
+  camera.position.z = settings.radius * Math.cos(MathThree.degToRad(settings.theta))
   camera.lookAt(scene.position)
   renderer.render(scene, camera)
   // composer.render(clock.getDelta())
@@ -47,10 +63,46 @@ const loop = function () {
   requestAnimationFrame(loop)
 }
 
+const initDatGui = function() {
+
+  const folderCamera = gui.addFolder('camera')
+  
+  folderCamera.add(settings, 'theta', -180, 180).onChange(function (val) {
+    if (!isNaN(val)) settings.theta = val
+  })
+  folderCamera.add(settings, 'radius', -40, 40).onChange(function (val) {
+    if (!isNaN(val)) settings.radius = val
+  })
+  folderCamera.add(settings, 'height', 0, 10).onChange(function (val) {
+    if (!isNaN(val)) settings.height = val
+  })
+  folderCamera.add(settings, 'autoRotate').onChange(function (val) {
+    settings.autoRotate = val
+  })
+
+  const folderPhysics = gui.addFolder('physics')
+  folderPhysics.add(settings, 'gx', -40, 40).onChange(function (val) {
+    if (!isNaN(val)) world.gravity.set(val, settings.gy, settings.gz)
+  })
+  folderPhysics.add(settings, 'gy', -40, 1).onChange(function (val) {
+    if (!isNaN(val)) world.gravity.set(settings.gx, val, settings.gz)
+  })
+  folderPhysics.add(settings, 'gz', -40, 40).onChange(function (val) {
+    if (!isNaN(val)) world.gravity.set(settings.gx, settings.gy, val)
+  })
+  folderPhysics.add(settings, 'restitution', 0.5, 2).onChange(function (val) {
+    if (!isNaN(val)) world.defaultContactMaterial.restitution = val
+  })
+  
+  gui.close()
+}
+
+
 initThree()
 initCannon()
 loop()
 addItems()
+initDatGui()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
