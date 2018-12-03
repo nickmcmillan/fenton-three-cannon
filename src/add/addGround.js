f1ca550911adcd93800ca548c59199cde4aca931
Nick McMillan <nick.a.mcmillan@gmail.com>
2018-12-03 13:44:33 +0100

    🚧 just messing with contact materials

--- src/add/addGround.js
+++ src/add/addGround.js
@@ -20,7 +20,18 @@ export const addGround = function () {
   // Create a plane
   // const shape = new CANNON.Plane() 
   const shape = new CANNON.Box(new CANNON.Vec3(size / 2, size / 2, 1))
-  const body = new CANNON.Body({ mass: 0, shape }) // mass = 0 which makes it static
+  // const groundMaterial = new CANNON.Material('groundMaterial')
+  // const material = new CANNON.ContactMaterial(
+  //   groundMaterial,
+  //   {
+  //     // friction: 10,
+  //     restitution: 2.5,
+  //     // contactEquationRelaxation: 11,
+  //     // contactEquationStiffness: 500,
+  //     // frictionEquationStiffness: 5,
+  //   }
+  // )
+  const body = new CANNON.Body({ mass: 0, shape, /*material*/ }) // mass = 0 which makes it static
   body.position.set(0, -1, 0)
   body.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
   world.add(body)
