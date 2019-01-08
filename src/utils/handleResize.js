869aa62a891e7d4f8753053abaa87138c46bb405
Nick McMillan <nick.a.mcmillan@gmail.com>
2019-01-09 00:13:15 +0100

    ✨ comment

--- src/utils/handleResize.js
+++ src/utils/handleResize.js
@@ -2,6 +2,7 @@ import { camera, renderer } from '../three'
 
 export default function () {
   camera.aspect = window.innerWidth / window.innerHeight
+  // update the camera's frustum
   camera.updateProjectionMatrix()
   //controls.handleResize()
   renderer.setSize(window.innerWidth, window.innerHeight)
